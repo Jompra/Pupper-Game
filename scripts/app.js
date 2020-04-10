a11220f5d4f575859c98e3469f1b7751c2c7eac6
George Jones <george@circleaudiovisual.com>
2020-04-10 12:34:05 +0100

    improved collision logic. refactoring required

--- scripts/app.js
+++ scripts/app.js
@@ -18,10 +18,22 @@ function init() {
   let furthestJump = 0
 
   //Obstacle Sprite Variables
-  let lineThirteenLead = 0
-
+  // TODO: find a better way of generating Zombie Initializer Lists. Harder Levels should have more!!!
+  //Zombie Array = [y position, x position, Sprite Identifier]
+  const initZombie = [[13, 7], [13, 10], [12, 7], [11, 7], [10, 7]]
 
   //Functions
+  // Randomises zombie Sprite number
+  //TODO make random number correct multiplier depending on number of sprites also refactor this so Math.Random is only called once!
+  function randomizeZombies(){
+    for (let i = 0; i <= initZombie.length - 1; i++){
+      const sprite = Math.ceil(Math.random() * 4)
+      initZombie[i].push(sprite)
+      console.log(initZombie[i])
+    }
+  }
+  randomizeZombies() // TODO Make this happen at a more appropriate time
+  
 
   //Adding Event Listeners
 
@@ -103,15 +115,19 @@ function init() {
     switch (event.keyCode) {
       case 37:
         moveLeft()
+        detectCollision()
         break
       case 38:
         moveUp()
+        detectCollision()
         break
       case 39:
         moveRight()
+        detectCollision()
         break
       case 40:
         moveDown()
+        detectCollision()
         break
     }
   }
@@ -131,55 +147,47 @@ function init() {
 
   //* Deals with obstical Movement
 
-  //takes a 2D array as [[x position, y position], [x position, y position]]
-  function initializeZombies(positions) {
-    console.log(positions)
-    positions.forEach(coords => {
-      console.log(coords)
-      cells[coords[0]][coords[1]].style.backgroundColor = 'red'
-    })
-  }
-
+  //Takes values in initZombie array and places them on the grid, also inc/decrements x value based on even/odd y value 
   function advanceZombies() {
     for (let i = 0; i < initZombie.length; i++) {
-      console.log(`initial positions = ${initZombie[i]}`)
-      cells[initZombie[i][0]][initZombie[i][1]].style.backgroundColor = ''
+      let direction = ''
+      cells[initZombie[i][0]][initZombie[i][1]].classList.remove('zombie')
+      cells[initZombie[i][0]][initZombie[i][1]].style.backgroundImage = ''
       if (initZombie[i][0] % 2 === 0 && initZombie[i][1] === width - 1){
         initZombie[i][1] = 0
+        direction = 'R'
       } else if (initZombie[i][0] % 2 === 0) {
+        direction = 'R'
         initZombie[i][1]++
       } else if (initZombie[i][0] % 2 !== 0 && initZombie[i][1] === 0) {
         initZombie[i][1] = width - 1
+        direction = 'L'
       } else {
         initZombie[i][1]--
+        direction = 'L'
       }
-      console.log(`advance positions = ${initZombie[i][1]}`)
-      cells[initZombie[i][0]][initZombie[i][1]].style.backgroundColor = 'red'
+      
+      cells[initZombie[i][0]][initZombie[i][1]].classList.add('zombie')
+      cells[initZombie[i][0]][initZombie[i][1]].style.backgroundImage = `url(../assets/zombies/${direction}_${initZombie[i][2]}.png)`
     }
+    detectCollision()
   }
 
-
-  // 
-  const initZombie = [[13, 7], [13, 10], [12, 7], [11, 7], [10, 7]]
-  initializeZombies(initZombie)
+  // calls the advance zombies in 1 second increments
+  // TODO: link speed multiplier to level dificulty
   setInterval(() => {
     advanceZombies(initZombie)
-  }, 1000)
+  }, 500)
 
-
-  // function advanceObstical() {
-  //   cells[13][lineThirteenLead].style.backgroundColor = ''
-  //   if (lineThirteenLead < width - 1){
-  //     lineThirteenLead++
-  //   } else {
-  //     lineThirteenLead = 0
-  //   }
-  //   cells[13][lineThirteenLead].style.backgroundColor = 'red'
-  // }
-
-
-
-  // setInterval(advanceObstical, 1000)
+  function detectCollision(){
+    if (cells[y][x].classList.contains('zombie')){
+      console.log(`x=${x} y=${y}`)
+      cells[y][x].classList.remove('sprite')
+      x = 7
+      y = 14
+      goSprite('start')
+    }
+  }
 
 
 }
